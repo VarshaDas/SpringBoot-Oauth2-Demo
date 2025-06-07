3d17e384d03350ae37916342b373af7b4161e1d5
Varsha <vdaskol@gmail.com>
2025-06-07 23:14:53 +0530

    oauth 2.0 code changes

---
+++ .oauth2-springdemo/src/main/java/com/codewithease/oauth2_springdemo/controllers/TestController.java
@@ -0,0 +1,36 @@
+package com.codewithease.oauth2_springdemo.controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "Public content accessible to everyone.";
+    }
+
+    @GetMapping("/private")
+    public String privateEndpoint() {
+        return "Private content accessible to authenticated users.";
+    }
+
+//    @GetMapping("/private")
+//    public String privateEndpoint(@AuthenticationPrincipal OAuth2User principal) {
+//        return "Private content for: " + principal.getAttributes().get("name");
+//    }
+//
+//    @GetMapping("/profile")
+//    public Map<String, Object> userProfile(@AuthenticationPrincipal OAuth2User principal) {
+//        return principal.getAttributes();
+//    }
+//
+//
+}
